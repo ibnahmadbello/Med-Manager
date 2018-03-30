69ad5d67f32c95c9c801d0a76dc3749c8e4e87c1
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-03-30 08:33:24 +0100

    Refactor/Rename file(s)

--- app/src/main/java/com/regent/tech/med_manager/LoginActivity.java
+++ app/src/main/java/com/regent/tech/med_manager/LoginActivity.java
@@ -3,7 +3,7 @@ package com.regent.tech.med_manager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
