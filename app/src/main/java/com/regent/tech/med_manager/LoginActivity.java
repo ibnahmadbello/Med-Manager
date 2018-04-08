d162428829bbbad45197969bf83f8301dad2a6a2
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-04-08 22:07:27 +0100

    Google Sign In Button

--- app/src/main/java/com/regent/tech/med_manager/LoginActivity.java
+++ app/src/main/java/com/regent/tech/med_manager/LoginActivity.java
@@ -1,32 +1,59 @@
 package com.regent.tech.med_manager;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-    public static final String TAG = LoginActivity.class.getSimpleName();
-    RelativeLayout layout;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener,
+        GoogleApiClient.OnConnectionFailedListener{
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 007;
+
+    private GoogleApiClient googleApiClient;
+    private ProgressDialog progressDialog;
+
+    private SignInButton signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        layout = (RelativeLayout) findViewById(R.id.login_layout);
-        setupSnackbar();
+        signInButton = (SignInButton) findViewById(R.id.btn_sign_in);
+
+        signInButton.setOnClickListener(this);
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        // Customizing the SignIn button
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+//        signInButton.setScopes(googleSignInOptions.getScopeArray());
 
     }
 
-
-    public void setupSnackbar(){
-        Snackbar.make(layout, "Health is Wealth", Snackbar.LENGTH_INDEFINITE)
-                .setAction("Sign In", null)
-                .setActionTextColor(Color.GREEN)
-                .show();
+    private void signIn(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    
 }
