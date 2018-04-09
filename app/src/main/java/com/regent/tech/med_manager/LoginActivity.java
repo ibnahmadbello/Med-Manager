c65e3a56214917a81a8a61fbb3d9f7fd78377c7e
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-04-09 21:44:29 +0100

    Menu Action for SignOut

--- app/src/main/java/com/regent/tech/med_manager/LoginActivity.java
+++ app/src/main/java/com/regent/tech/med_manager/LoginActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -144,5 +146,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu options from the res/menu/menu_login.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Sign out" menu option
+            case R.id.sign_out:
+                // TODO
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
